2fb1df5fb285e3ff6fd144b6dd5ddee6c6fc8a27
edecambra <edecambra@gmail.com>
2014-04-24 22:35:06 -0400

    fourth commit

--- main_data_transforms.R
+++ main_data_transforms.R
@@ -1,14 +1,18 @@
 
 library(foreign)
-library(doBy)
 library(ff)
 library(ffbase)
-setwd("C:/Users/Edward/Desktop/Research_Files/InequalityResearch") #desktop local drive
+#setwd("C:/Users/Edward/Desktop/Research_Files/InequalityResearch") #desktop local drive
+#setwd("C:/Users/edecambr/Desktop/Research_Files/InequalityResearch")
+#setwd("C:/Users/edecambr/Dropbox/Research_Files/InequalityResearch") #laptop
+setwd("C:/Users/Edward/Dropbox/Research_Files/InequalityResearch") #desktop
 
 
-# To load the data into memory, uses compressed gzip file: main.csv.gz 
+## To load the data into memory, uses compressed gzip file: main.csv.gz 
+## Loading a Flat File using ff commands
 ptm <- proc.time()
 c <- read.csv("main.csv.gz", nrows = 100000)  #Using a large nrow allows R the appropriate Class determination
+
 class <- sapply(c, class)
 class[grep("logical", class)] <- "factor"
 class[grep("integer", class)] <- "factor"
@@ -16,28 +20,33 @@ class[grep("numeric", class)] <- "double"
 main.ff <- read.csv.ffdf(file = "main.csv.gz", first.rows = 100000, colClasses = class)
 proc.time() - ptm  #takes roughly 8 minutes to process, 
 
-#Date Coding
+## Processing the Files
 main.ff$start_date <- with(main.ff, as.Date(start_date, "%d%b%Y"))
-main.ff$end_date <- with(main.ff, as.Date(end_date, "%d%b%Y"))
-main.ff$year <- with(main.ff, as.integer(format(start_date, "%Y")))
-main.ff$fips <- with(main.ff, as.integer(fips))
+main.ff$year <- with(main.ff, as.factor(format(start_date, "%Y")))
+main.ff$fips <- with(main.ff, as.factor(fips))
+#main.ff$KeyColumn <- ikey(main.ff[c("fips", "year")])
+main.ff<- main.ff[fforder(main.ff$fips, main.ff$year), ]
+
+#agg.func <- function(x){
+ # agg.df <- tapply(x$spend, list(x$fips, x$year), FUN = sum)
+#}
+
 system.time(agg <- ffdfdply(x = main.ff, 
-                split = main.ff$fips,
+                split = main.ff$year,
                 FUN = function(x){
-                  require(doBy)
-                  summaryBy(spend~year + fips, data = x, keep.names = T, FUN = sum, na.rm = T)
-                  },
+                  aggregate(spend~fips + year, data = x, FUN = sum, na.rm = T)
+                },
                 BATCHBYTES = 10000, trace = T))
 agg.tab <- as.data.frame(agg)                
 
 
-
-state.codes <- read.csv(file = "statemastercodelist.csv")
+## Supplimental Data
+state.codes <- read.csv(file = "statemastercodelist.csv.gz")
 state.codes <- data.frame(state.codes$state_frank, state.codes$fipsmil)
 names(state.codes) <- c("state", "fips")
 frank <- read.csv(file = "Frank_All_v96a.csv")
-inflation <- read.csv(file = "inflation.csv")
-extra.state <- read.csv("extra.state.csv")
+inflation <- read.csv(file = "inflation.csv.gz")
+extra.state <- read.csv("extra.state.csv.gz")
 vars <- c("usspend", "pop", "nom.spend", "nomGSP", "deflator", "emp.rate", "year", "fipsmil"  )
 extra.state <- extra.state[,vars]
 colnames(extra.state) <- c("usspend", "pop", "nom.spend", "nom.gsp", "deflator", "emp.rate", "year", "fips"  )
@@ -45,12 +54,8 @@ colnames(extra.state) <- c("usspend", "pop", "nom.spend", "nom.gsp", "deflator",
 suppliment <- merge(frank, state.codes, by = "state", all = TRUE)
 suppliment <- merge(suppliment, inflation, by = "year", all = TRUE)
 suppliment <- merge(suppliment, extra.state, by = c("fips", "year"), all = TRUE)
-write.csv(suppliment, file = "suppliment.csv")
-system("c:/Rtools/bin/gzip suppliment.csv")
-system("c:/Rtools/bin/gzip -d suppliment.csv.gz")
 
 data <- merge(agg.tab, suppliment, by = c("fips", "year"), all = TRUE)
-
-
+write.csv(data, file="inequalitydata.csv")
 
 
